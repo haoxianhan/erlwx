77043689e0f91ed702330d8ae894772f40369e15
haoxian <haoxianhan@gmail.com>
2020-03-26 11:46:37 +0800

    unify format

--- src/erlwx.erl
+++ src/erlwx.erl
@@ -20,8 +20,12 @@ code_2_session(AppId, Secret, Js_Code) ->
 	erlwx_api:code_2_session(AppId, Secret, Js_Code).
 
 check_signature(RawData, Signature1, SessionKey) ->
-	Signature2 = erlwx_util:sha1(RawData ++ SessionKey),
-	Signature1 == Signature2.
+	case erlwx_util:sha1(RawData ++ SessionKey) of
+		Signature2 when Signature1 == Signature2 ->
+			{ok};
+		_ ->
+			{error, ?ERLWX_ERROR_CHECK_SIGN, check_sign_error}
+	end.
 
 check_wx_biz_data_crypt(AppId, SessionKey, Iv, EncryptedData) ->
 	case wx_biz_data_crypt:decrypt(AppId, SessionKey, Iv, EncryptedData) of
